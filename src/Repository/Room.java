dfa3e689a1be584332a000fff89105569fc2c348
EunsilSon <eunsil1023@gmail.com>
2020-11-26 15:37:55 +0900

    Netty 버전의 Room

--- src/Repository/Room.java
+++ src/Repository/Room.java
@@ -1,50 +1,90 @@
 package Repository;
 
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.util.concurrent.GlobalEventExecutor;
 import org.json.simple.JSONObject;
 
 import java.util.ArrayList;
 
-public class Room { //게임 방
+public class Room { // 게임 방
 
-    private static ArrayList<Room> rooms = new ArrayList<>();
-
-    int roomCode; //
+    ChannelGroup channels;
     ArrayList<Crewmate> crewmates; // 참가자들
+    private static ArrayList<Room> rooms = new ArrayList<>();
+    int roomCode;
 
-    public Room(int roomCode){
+    public Room(int roomCode) {
         this.roomCode = roomCode;
         this.crewmates = new ArrayList<>();
+        this.channels = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
     }
 
-    public int getRoomCode(){
-        return roomCode;
+    public ChannelGroup getChannelGroup() { return channels; }
+
+    public static ArrayList<Room> getRooms() { return rooms; }
+
+    // !
+    public ArrayList<Crewmate> getCrewmates() { return crewmates; }
+
+    public int getRoomCode() { return roomCode; }
+
+    //!
+    public static Room getRoomByCode(int code) {
+        for (Room room : rooms) {
+            if (room.getRoomCode() == code)
+                return room;
+        }
+        return null;
     }
 
-    public ArrayList<Crewmate> getCrewmates(){
-        return crewmates;
+    public void enter(ChannelHandlerContext ctx, JSONObject json) {
+        crewmates.add(new Crewmate(json));
+        channels.add(ctx.channel());
     }
 
-    public static Room getRoom(){
-        if(rooms.size() == 0) // 방 하나도 없음
-            rooms.add(new Room(0));
+    public static Room getMyRoomByChannel(Channel channel) {
+        for(int i = 0; i < rooms.size(); i++) {
+            if(rooms.get(i).getChannelGroup().contains(channel)) {
+                return rooms.get(i);
+            }
+        }
+        return null;
+    }
+
+    // 방 생성 + 있는 방 리턴
+    public static Room selectRoom() {
+        if(rooms.size() == 0) { // 방 하나도 없음
+            rooms.add(new Room(1));
+        }
 
         for(Room room : rooms)
-            if(room.crewmates.size() < 5) // 만들어진 방 중에 자리 있으면 그 방 리턴
+            if(room.channels.size() < 5) {// 만들어진 방 중에 자리 있으면 그 방 리턴
                 return room;
+            }
 
-        if(rooms.get(rooms.size()-1).crewmates.size() >= 5){ // 방 다 꽉참
-            int temp = rooms.get(rooms.size()-1).roomCode; // 끝방 코드 번호 가져옴
-            rooms.add(new Room(temp + 1)); // 끝에 방 하나 새로 생성
+        if(rooms.get(rooms.size()-1).channels.size() >= 5) { // 방 다 꽉참
+            rooms.add(new Room(rooms.get(rooms.size()-1).roomCode)); // 마지막 코드+1 번호로 방 하나 새로 생성
         }
 
         return rooms.get(rooms.size()-1); // 끝방 리턴
     }
 
-    public void update(JSONObject requestJson) { // 방 정보 업데이트
-        for(Crewmate crewmate : crewmates){
-            if( crewmate.owner.equals(requestJson.get("owner")) ){
+    // 방 정보 업데이트
+    public void update(JSONObject requestJson) {
+        for(Crewmate crewmate : crewmates) {
+            if(crewmate.owner.equals(requestJson.get("owner"))) {
                 crewmate.update(requestJson);
+                crewmate.rate = 1;
+            } else {
+                crewmate.rate++;
             }
+
+            if(crewmate.rate > 1000)
+                crewmates.remove(crewmate);
+            //System.out.println(crewmate.getInitCrewmateJson().toJSONString());
         }
     }
 }
